c37d5ac74532d5ef5414b04626e1b17a03c76858
Jacqueline Chenault <adigitalnative@gmail.com>
2018-11-02 21:48:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    let inputYear = new Date(year)
+
+    return (inputYear.getFullYear() - (this.startDate.getFullYear() - 1))
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    let verticalTravel = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    
+    let horizontalTravel = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+
+    return horizontalTravel + verticalTravel
+  }
+
+  estimatedTime(peakHoursBoolean) {
+    if (peakHoursBoolean) {
+      return (this.blocksTravelled() / 2)
+    } else {
+      return (this.blocksTravelled() / 3)
+    }
+  }
+}
